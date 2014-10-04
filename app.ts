6e598b4aca42302d09d7806a8e0a5c8ff915387c
vivainio@gmail.com <vivainio@gmail.com>
2014-10-04 22:30:46 +0300

    +

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 /// <reference path="typings/js-yaml/js-yaml.d.ts" />
+/// <reference path="typings/lodash/lodash.d.ts" />
 
 
 import yaml = require("js-yaml");
@@ -6,6 +7,15 @@ import os = require("os");
 import ps = require("child_process");
 import fs = require("fs");
 import _ = require("lodash");
+import path = require("path");
+import util = require("util");
+
+var rootpath = "c:/t";
+
+interface ComponentEntry {
+    t?: string;
+    repo?: string;
+};
 
 class Cm {
 
@@ -13,11 +23,15 @@ class Cm {
         var cont = fs.readFileSync("test.yaml").toString();
         var o = yaml.load(cont);
         console.log(o);
-        
+        _.forEach(o.components, (e: ComponentEntry) => {
+            var pth = path.join(rootpath, e.t);
+            process.chdir(rootpath)
+            var cmd = util.format("git clone %s %s", e.repo, e.t);
+            console.log(cmd);
+         });        
     }
 
     checkOut() {
-
     }
    
 };
